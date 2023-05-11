bb76d6ae36aceb2b9369377d395f0ffb9f14e29a
learndevops-083 <learndevops083@gmail.com>
2023-05-11 19:47:18 +0100

    update tf config files

--- eks-worker-nodes.tf
+++ eks-worker-nodes.tf
@@ -119,9 +119,9 @@ resource "aws_launch_configuration" "eks" {
 }
 
 resource "aws_autoscaling_group" "eks" {
-  desired_capacity     = 5
+  desired_capacity     = 2
   launch_configuration = "${aws_launch_configuration.eks.id}"
-  max_size             = 5
+  max_size             = 2
   min_size             = 1
   name                 = "eks"
   vpc_zone_identifier  = aws_subnet.eks.*.id
