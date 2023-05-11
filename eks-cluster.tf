071ecf2c82910c2e0f06023fe607d7afc5bcb0c6
learndevops-083 <learndevops083@gmail.com>
2023-05-11 13:07:21 +0100

    update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -51,13 +51,15 @@ resource "aws_security_group_rule" "eks-cluster-ingress-node-https" {
   type                     = "ingress"
 }
 
-resource "aws_eks_cluster" "eks" {
-  name     = "${var.cluster-name}"
-  role_arn = "${aws_iam_role.eks-cluster.arn}"
+  resource "aws_eks_cluster" "eks" {
+  name     = var.cluster-name
+  role_arn = aws_iam_role.eks-cluster.arn
+
   vpc_config {
-    security_group_ids = ["${aws_security_group.eks-cluster.id}"]
-    subnet_ids         = ["${aws_subnet.eks.*.id}"]
+    security_group_ids = [aws_security_group.eks-cluster.id]
+    subnet_ids         = aws_subnet.eks.*.id
   }
+
   depends_on = [
     aws_iam_role_policy_attachment.eks-cluster-AmazonEKSClusterPolicy,
     aws_iam_role_policy_attachment.eks-cluster-AmazonEKSServicePolicy,
